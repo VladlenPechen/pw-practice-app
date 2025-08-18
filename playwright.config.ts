c1ed0454d665d55b6bcfa6973221c5503fa33cdc
VladlenPechen <ensigner@gmail.com>
2025-08-18 13:04:14 +0200

    playwrght-config changes (webserver )

--- playwright.config.ts
+++ playwright.config.ts
@@ -88,8 +88,8 @@ export default defineConfig<TestOptions>({
       }
     }
   ],
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:4200'
-  // }
+   webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:4200'
+  }
 });
